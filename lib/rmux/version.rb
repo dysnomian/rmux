cd16d9234a442d707a027766b126d134be397ee1
Liss McCabe <liss@eristiccode.com>
2019-05-24 14:08:45 -0700

    :sparkles: brand new gem

---
+++ lib/rmux/version.rb
@@ -0,0 +1,3 @@
+module Rmux
+  VERSION = "0.1.0"
+end
---
+++ spec/rmux_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rmux do
+  it "has a version number" do
+    expect(Rmux::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
